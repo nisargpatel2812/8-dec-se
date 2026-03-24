395c608bf16fa10df3732da7c4bc81af51492abe
nisargpatel2812 <nisarg2945@gmail.com>
2026-03-24 14:04:50 +0530

    new..

---
+++ firstprogram.c
@@ -0,0 +1,85 @@
+//first program in c language
+#inlude<stdio.h>
+void main()
+{
+    printf("hello");
+    
+    printf("welome");
+    
+    printf("hii");
+
+}
+
+----------------------------------------------------------------
+
+printf() :
+
+-- in c lang print f is a function is used to display msg on output
+
+---------------------------------------------------------------------
+
+syntax :
+
+printf("msg");
+
+; :- semicolon is indicate the end of line
+
+-- it is used to terminate the statement
+-- by default print f function join with previous function
+
+-----------------------------------------------------------------
+
+escape sequence:-
+
+/n : new line
+/t : new tab
+/' : to display single quat...
+/" : double quat....
+
+-------------------------------------------------------------------------
+
+datatype:- which type of value is store in variables is called datatype.
+
+
+                                                                              values
+                                                                                 |
+ 
+ 
+
+
+--> numbers :- without point is called int(intger)(%d)
+           :- with point is called float,double,long double
+
+
+
+--> character :- single char is shown char(%c), e.g. 'a'
+              :- string char shown as char[size](%c)
+
+
+
+--> yes/no :- Boolean, true(1) & false(0) , bool
+
+-----------------------------------------------------------------
+
+variable:- var is a name which store a value.
+
+-- in c lang we can take any name as a var.
+-- but there are some rules we have to follow
+
+variable divide in 3 steps:
+
+1)VARIABLE DECLARATION :-
+
+-- before use of var we must behave to declare datatype
+
+syntax : <datatype>  varname;
+
+2) VAR INITIALIZATION :-
+
+-- assign a value
+
+syntax : varname = value;
+
+3)  VAR CALLING OR USE:
+
+syntax : printf(varname)
